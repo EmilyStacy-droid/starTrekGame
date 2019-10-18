5e27763bebbe22b0675730b503117c58bcfbca1f
Emily Stacy <Emily.Stacy@improving.com>
2019-10-18 13:01:00 -0500

    sector

--- src/main/java/org/improving/starTrekGame/Sector.java
+++ src/main/java/org/improving/starTrekGame/Sector.java
@@ -19,8 +19,7 @@ public class Sector {
 
     public void displaySector() {
         for (int i = 0; i < grid.length; i++) {
-            System.out.print("\n");
-            for (int j = 0; j < grid[i].length; j++) {
+            System.out.print("\n");            for (int j = 0; j < grid[i].length; j++) {
                 System.out.print(grid[i][j] + "\t");
             }
         }
