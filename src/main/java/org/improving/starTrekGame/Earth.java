cdf996e1943b7f11737cc3c30bae9e9d066fb6c7
John Palasota <John.Palasota@improving.com>
2019-10-18 10:04:00 -0500

    Fixed Earth -- game.intro() added as well.

--- src/main/java/org/improving/starTrekGame/Earth.java
+++ src/main/java/org/improving/starTrekGame/Earth.java
@@ -2,6 +2,11 @@ package org.improving.starTrekGame;
 
 public class Earth {
     public static void main(String[] args) {
+        Game game = new Game();
+        Sector sector = new Sector();
+        String[][] middleSector = sector.fillArray(new String[10][10]);
 
+        game.intro();
+        sector.displaySector(middleSector);
     }
 }
