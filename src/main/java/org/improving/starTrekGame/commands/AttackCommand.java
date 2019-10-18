dad132f98d79f11bfe876219c71c2ffa646b21f2
Siri Bafna <Siri.Bafna@improving.com>
2019-10-18 14:47:42 -0500

    k

--- src/main/java/org/improving/starTrekGame/commands/AttackCommand.java
+++ src/main/java/org/improving/starTrekGame/commands/AttackCommand.java
@@ -2,7 +2,7 @@ package org.improving.starTrekGame.commands;
 
 import org.improving.starTrekGame.Sector;
 import org.improving.starTrekGame.Ship;
-
+//h
 public class AttackCommand {
     private final Sector sector;
     private final Ship target;
